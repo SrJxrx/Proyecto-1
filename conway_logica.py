0821fa6fe134dbe076ae1b3b6d81931ecbe9163a
SrJxrx <136137294+SrJxrx@users.noreply.github.com>
2026-06-03 13:43:04 -0600

    Delete matrix generation functions
    Removed functions for generating random and empty matrices.

--- conway_logica.py
+++ conway_logica.py
@@ -1,21 +1,6 @@
 from random import randint
 from copy import deepcopy
 
-def generar_matriz_aleatoria(filas, columnas):
-    """Función que retorna una matriz de las dimensiones
-    especificadas con valores enteros aleatorios de 0 o 1
-    Entradas:filas, columnas
-    Salidas: matriz con valores enteros aleatorios de 0 o 1
-    Restrincciones:numeros enteros mayores a 0"""
-    return [[randint(0, 1) for c in range(columnas)] for f in range(filas)]    
-
-def generar_matriz_vacia(filas, columnas):
-    """Funcion que crea una matriz a base de las dimensiones dadas, llenada con ceros.
-    Entradas: filas, columnas
-    Salidas: matriz llenada con ceros
-    Restrincciones:numeros enteros mayores a 0"""
-    return [[0 for c in range(columnas)] for f in range(filas)]
-
 def obtener_vecinos(M, f, c):
     """Función que retorna una lista con los estados de
     los 8 vecinos de la célula en la posición f, c de M.
