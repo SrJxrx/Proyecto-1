604bcb52d8fd7b9dcf275fedc62cbd66d0fe8089
SrJxrx <136137294+SrJxrx@users.noreply.github.com>
2026-06-03 13:57:23 -0600

    Refactor cell transition functions and remove unused parameters

--- conway_logica.py
+++ conway_logica.py
@@ -1,63 +1,52 @@
 from random import randint
 from copy import deepcopy
 
-def obtener_vecinos(M, f, c):
-    """Función que retorna una lista con los estados de
-    los 8 vecinos de la célula en la posición f, c de M.
-    Entradas: Matriz, fila,columna
-    Salidas: lista con los estados de los vecinos de la celula dada
-    Restrincciones:"""
-    vecinos = []
-    filas = len(M)
-    columnas = len(M[0])
+def generar_matriz(filas, columnas):
+    """Función que retorna una matriz de las dimensiones
+    especificadas con valores enteros aleatorios de 0 o 1"""
+    return [[randint(0, 1) for c in range(columnas)] for f in range(filas)]    
 
-    for i in [-1, 0, 1]:
-        for j in [-1, 0, 1]:
+def obtener_vecinos(M, f, c):
+
+    vecinos = []
+
+    fs = len(M)
+    cs = len(M[0])
+
+    for i in range(-1, 2):
+        for j in range(-1, 2):
             if i == 0 and j == 0:
                 continue
-            f_vecino = (f + i) % filas
-            c_vecino = (c + j) % columnas
-            
-            vecinos.append(M[f_vecino][c_vecino])
+            nueva_fila = (f + i) % fs
+            nueva_columna = (c + j) % cs
+            vecinos.append(M[nueva_fila][nueva_columna])
 
     return vecinos
 
-def transicion_celula(estado, vecinos, reglas_b, reglas_s):
+def transicion_celula(estado, vecinos):
     """Retorna el nuevo estado de la célula de acuerdo
     al estado de sus vecinos.
     Si estado == 0 y tiene 3 vecinos vivos --> viva
     Si estado == 1 y tiene menos de 2 vecinos vivos --> muere
     Si estado == 1 y tiene más de 3 vecinos vivos --> muere
-    Cualquier otra combinación, el estado sigue igual.
-    Entradas: estado, vecinos, reglas_b, reglas_s
-    Salidas: 0 o 1
-    Restrincciones:Estado debe ser binario, vecinos debe ser una lista al igual que las reglas dadas."""
-    vecinos_vivos = sum(vecinos)
+    Cualquier otra combinación, el estado sigue igual."""
     sumatoria = vecinos.count(1)
-    if estado == 0:
-        if sumatoria in reglas_b:
-            return 1
-        return 0
+    if estado == 0 and sumatoria == 3:
+        estado = 1
+    elif estado == 1 and sumatoria <2 or estado == 1 and sumatoria > 3:
+        estado = 0         
+    return estado
 
-    if estado == 1:
-        if sumatoria in reglas_s:
-            return 1
-        return 0
-
-
-def transicion(M, reglas_b, reglas_s):
+def transicion(M):
+    #deepcopy de matriz
     """Toma a la matriz completa y le aplica la función de
     transición a cada célula con su propio vecindario y deja
-    el resultado en una matriz nueva.
-    Entradas: M, lista de relgas b y s
-    Salidas: Crea una matriz nueva a partir de las reglas y la matriz dada
-    Restrincciones:M debe ser una matriz y las reglas s y b deben ser listas.
-    """
+    el resultado en una matriz nueva."""
     nuevaM = deepcopy(M)
-
     for i in range(len(M)):
         for j in range(len(M[0])):
-            vecinos = obtener_vecinos(M, i, j)
-            nuevaM[i][j] = transicion_celula(M[i][j], vecinos, reglas_b, reglas_s)
-
+           vecinos = obtener_vecinos(M, i, j)
+           transicion_celula(M[i][j], vecinos)
+           nuevaM[i][j] = transicion_celula(M[i][j], vecinos)
+     
     return nuevaM
